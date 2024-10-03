85ebccdd5ed8994df487678e9d312127e8b2e2f6
Mateusz Matyszczak <36937475-matyszczakmateu@users.noreply.replit.com>
2024-10-03 12:00:46 +0000

    Dodanie eventu polegajacego na wyswietleniu liczb od 1 do 9 oddzielonych przecinkiem po wcisnieciu przycisku o ID ex1_button

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-(function () {
-  //TODO
-})();
+const element1 = document.getElementById("ex1_button");
+element1.addEventListener("click", ex1Click);
+
+function ex1Click() {
+  for (let i = 0; i < 10; i++) {
+    document.getElementById("ex1_content").innerHTML += "" + i;
+    if (i < 9) {
+      document.getElementById("ex1_content").innerHTML += ", ";
+    }
+  }
+}
